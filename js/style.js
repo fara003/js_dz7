939a615bbc4884e0bebd626f51bea9141dd69479
fara003 <109841439+fara003@users.noreply.github.com>
2023-02-05 12:46:20 +0300

    Add files via upload

---
+++ js/style.js
@@ -0,0 +1,18 @@
+//let names = ['Aleksey', 'Kartoshka', 'Margarita' , 'Morgenshtern', 'Monica']
+// у вас будет массив надо туда добавить через 3 промта 3 числа и отсортировать массив
+
+
+let num1=prompt("1")
+let num2=prompt("2")
+let num3=prompt("3")
+let num=[num1,num2,num3]
+
+alert(num.sort((a,b)=>{return a-b}));
+
+
+
+
+
+
+
+
